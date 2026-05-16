cdaaaa5c652c38c3fa005d8e299378ade42e7179
Harshita Ghughutiyal <124561481+Itsmexplorer18@users.noreply.github.com>
2026-05-16 21:22:59 +0530

    initial commit

---
+++ functions/lambdas.cpp
@@ -0,0 +1,10 @@
+int main(){
+  /*lambda expression will have [capture clause](parameters){
+  and the body here
+};
+the capture clause is to capture the variables and functions within the curr scope
+& is used for passing those variables by reference 
+= is used for passing by value you cannot modify them if you pass  by value
+*/
+  return 0;
+}
