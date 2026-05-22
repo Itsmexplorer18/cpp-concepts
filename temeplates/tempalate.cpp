0c0c8c7773a96c4b574cd25e9586cf98a5f9e39c
Harshita Ghughutiyal <124561481+Itsmexplorer18@users.noreply.github.com>
2026-05-22 19:54:56 +0530

    simple example

---
+++ temeplates/tempalate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+T add(T x,T y){
+    return x+y;
+}
+template <typename T1,typename T2>
+T2 add_diff(T1 x,T2 y){
+    return x+y;
+}
+int main() {
+	// your code goes here
+   cout<<add(3,4)<<endl;
+   cout<<add(2.4,5.9)<<endl;
+   cout<<add_diff(3,4.5)<<endl;
+   return 0;
+
+}
